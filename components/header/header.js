f18e5e76ec46026fbd4db3219e7460d269cb63d0
Nestor Mosquera <yunsde18@gmail.com>
2022-03-07 13:28:39 -0500

    new links

--- components/header/header.js
+++ components/header/header.js
@@ -8,13 +8,13 @@ export default function Header() {
         <header className={styles.header}>
             <button className={styles.openMenu} onClick={() => setOpenMenu(!openMenu)}><i className='bx bx-menu'></i></button>
             <nav className={!openMenu? styles.navActvie : ""}>
-                <Link href="https://rvegaming.vercel.app"><a><img src="./2rveg.svg" alt="" /></a></Link>
+                <Link href="/"><a><img src="./2rveg.svg" alt="" /></a></Link>
                 <Link href="/"><a>Inicio</a></Link>
-                <Link href=""><a>Premium</a></Link>
+                <Link href="/"><a>Premium</a></Link>
                 <Link href="/tournament"><a>Torneos</a></Link>
                 <Link href=""><a>Blog</a></Link>
             </nav>
-            <Link href="https://rvegaming.vercel.app"><a className={styles.logo}><img src="./2rveg.svg" alt="" /></a></Link>
+            <Link href="/"><a className={styles.logo}><img src="./2rveg.svg" alt="" /></a></Link>
             <div>
                 <Link href="/register"><a className={styles.iconBtn}><i className='bx bxs-user-circle' ></i></a></Link>
                 <Link href="/register"><a className={styles.btn}>Registrarse</a></Link>
